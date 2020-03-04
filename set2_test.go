b619c631bd79063620983ca88270871ddcc0017b
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2020-03-04 16:32:59 +0530

    Tests - Set 2 Problem 2

--- set2_test.go
+++ set2_test.go
@@ -2,6 +2,7 @@ package cryptopals
 
 import (
 	"bytes"
+	"crypto/aes"
 	"testing"
 )
 
@@ -12,4 +13,18 @@ func TestProblem9(t *testing.T) {
 	if res := padPKCS7([]byte("YELLOW SUBMARINE"), 20); !bytes.Equal(res, []byte("YELLOW SUBMARINE\x04\x04\x04\x04")) {
 		t.Errorf("%q", res)
 	}
+}
+
+func TestProblem10(t *testing.T) {
+	msg := []byte("YELLOW SUBMARINEYELLOW SUBMARINE")
+	iv := make([]byte, 16)
+	b, _ := aes.NewCipher([]byte("YELLOW SUBMARINE"))
+	res := decryptCBC(encryptCBC(msg, b, iv), b, iv)
+	if !bytes.Equal(res, msg) {
+		t.Errorf("%q", res)
+	}
+
+	data := decodeBase64(t, string(readFile(t, "data/10.txt")))
+	decoded := decryptCBC(data, b, iv)
+	t.Logf("Result: %s", decoded)
 }
