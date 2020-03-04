d3962b7b80ae4b8b669a40e3a3947881d857270f
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2020-03-04 21:21:46 +0530

    Set 2 Problem 12

--- set2.go
+++ set2.go
@@ -1,9 +1,12 @@
 package cryptopals
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"fmt"
+	"log"
 	mathrand "math/rand"
 	"time"
 )
@@ -93,4 +96,73 @@ func newECBCBCOracle() func([]byte) []byte {
 		}
 		return encryptECB(msg, b)
 	}
+}
+
+func newECBSuffixOracle(secret []byte) func([]byte) []byte {
+	key := make([]byte, 16)
+	rand.Read(key)
+
+	b, _ := aes.NewCipher(key)
+	return func(in []byte) []byte {
+		time.Sleep(200 * time.Microsecond)
+		msg := append(in, secret...)
+		return encryptECB(padPKCS7(msg, 16), b)
+	}
+}
+
+func recoverECBSuffix(oracle func([]byte) []byte) []byte {
+	var bs int
+
+	for blockSize := 2; blockSize < 100; blockSize++ {
+		msg := bytes.Repeat([]byte{42}, blockSize*2)
+		// msg = append(msg, 6)
+		if detectAESECB(oracle(msg)[:blockSize*2], blockSize) {
+			bs = blockSize
+			break
+		}
+	}
+	if bs == 0 {
+		panic("couldn't detect block size")
+	}
+	log.Printf("bs: %d", bs)
+
+	buildDict := func(known []byte) map[string]byte {
+		dict := make(map[string]byte)
+
+		msg := bytes.Repeat([]byte{42}, bs)
+		msg = append(msg, known...)
+		msg = append(msg, '?')
+		msg = msg[len(msg)-bs:]
+
+		for b := 0; b < 256; b++ {
+			msg[bs-1] = byte(b)
+			res := string(oracle(msg)[:bs])
+			dict[res] = byte(b)
+		}
+		return dict
+	}
+
+	dict := buildDict(nil)
+	msg := bytes.Repeat([]byte{42}, bs-1)
+	res := string(oracle(msg)[:bs])
+	firstByte := dict[res]
+	fmt.Printf("First byte: %c / %v\n", firstByte, firstByte)
+
+	var plaintext []byte
+	for i := 0; i < len(oracle([]byte{})); i++ {
+		dict := buildDict(plaintext)
+		msg := bytes.Repeat([]byte{42}, mod(bs-i-1, bs))
+		skip := i / bs * bs
+		res := string(oracle(msg)[skip : skip+bs])
+		plaintext = append(plaintext, dict[res])
+
+		fmt.Printf("%c", dict[res])
+	}
+	fmt.Printf("\n")
+
+	return nil
+}
+
+func mod(a, b int) int {
+	return (a%b + b) % b
 }
